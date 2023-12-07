004458317b2d70504fb5e581c33b05855c0c405e
LongIch2810 <ichtrachuylong.91.04@gmail.com>
2023-12-07 11:33:25 +0700

    upload lan 2

---
+++ assets/JS/LoginLogout/Information.js
@@ -0,0 +1,24 @@
+let index = parseInt(localStorage.getItem('index'));
+console.log(index);
+if (index !== null) {
+  document.querySelector("p .username").innerHTML = `${
+    JSON.parse(localStorage.getItem("UsersInfo"))[index].lastNameUser
+  } ${
+    JSON.parse(localStorage.getItem("UsersInfo"))[index].firstNameUser
+  } !`;
+  document.querySelector("p #username").innerHTML = `${
+    JSON.parse(localStorage.getItem("UsersInfo"))[index].lastNameUser
+  } ${
+    JSON.parse(localStorage.getItem("UsersInfo"))[index].firstNameUser
+  } !`;
+  document.querySelector("p .username").style.fontSize = "14px";
+  document.querySelector("p .username").style.fontWeight = "bold";
+
+  document.querySelector("p #username").style.fontSize = "14px";
+  document.querySelector("p #username").style.fontWeight = "bold";
+
+  document.querySelector("div .phoneuser").innerHTML = `${
+    JSON.parse(localStorage.getItem("UsersInfo"))[index].phoneUser
+  }`;
+  document.querySelector("div .phoneuser").style.fontSize = "14px";
+}
--- assets/JS/LoginLogout/formlogin.js
+++ assets/JS/LoginLogout/formlogin.js
@@ -1,5 +1,6 @@
 const putOut = document.querySelector(".form-login .login .btn");
-
+let index_email;
+let index_password;
 let booleanEmailLogin = false;
 let booleanPasswordLogin = false;
 
@@ -9,8 +10,9 @@ putOut.addEventListener("click", (e) => {
   const emailLogin = document.querySelector("#email");
   const passwordLogin = document.querySelector("#password");
   checkLo(emailLogin, passwordLogin);
-  if (booleanEmailLogin && booleanPasswordLogin) {
-    document.location.href="http://127.0.0.1:5500/mainpage.html";
+  if (booleanEmailLogin && booleanPasswordLogin && index_email===index_password) {
+    document.location.href="http://127.0.0.1:5500/DoAnWeb-main/views/web/Information.html";
+    localStorage.setItem("index",JSON.stringify(index_email));
   } else {
     document.querySelector(".notification").innerHTML =
       "<i class='bx bx-error-circle'></i> Email hoặc mật khẩu không chính xác";
@@ -32,17 +34,19 @@ function checkLo(emailLogin, passwordLogin){
       emailLogin.value
     ) {
       booleanEmailLogin = true;
+      index_email=i;
     } else {
       booleanEmailLogin = false;
     }
     if(JSON.parse(localStorage.getItem("UsersInfo"))[i].passwordUser ===
     passwordLogin.value) {
       booleanPasswordLogin = true;
+      index_password=i;
     }
     else {
       booleanPasswordLogin = false;
     }
-    if(booleanEmailLogin == true && booleanPasswordLogin == true){
+    if(booleanEmailLogin == true && booleanPasswordLogin == true && index_email===index_password){
       break;
     }
   }
